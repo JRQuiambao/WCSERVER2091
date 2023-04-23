2e90c4ea31d80b6b54d2e83fa8d31d2ce3fa66bd
JRQuiambao <126967051+JRQuiambao@users.noreply.github.com>
2023-04-23 20:38:32 +0800

    Add files via upload

---
+++ finals/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<link rel="stylesheet" href="bootstrap.min.css">
+<router-link class="nav-link" to="/blog">Blog</router-link>
---
+++ finals/src/blog.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="blog">
+      <h1>{{ title }}</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'blog',
+    data (){
+        return{
+            title: 'Blog'
+        }
+    }
+  }
+  </script>
